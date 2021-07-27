149e67c43c68f02c29035ecb871220d9e8428f83
dat nguyen <thanhdat.itdn@gmail.com>
2021-07-27 23:46:15 +0700

    Landscape Issue

--- src/app/pages/Artwork/FormatPage/index.tsx
+++ src/app/pages/Artwork/FormatPage/index.tsx
@@ -3,9 +3,12 @@ import { FormatPageMobile } from './FormatPageMobile';
 import { useResponsive } from 'utils/responsive';
 import { Prompt } from 'react-router-dom';
 import { RELOAD_WARNING } from 'app/helpers/constants';
+import { useLandscape } from 'hooks';
+import { WarningPopup } from '../Popup';
 
 export const FormatPage = () => {
   const { isMobile } = useResponsive();
+  const { isOrientation } = useLandscape();
 
   const confirmationHandler = (location, action) => {
     if (action !== 'PUSH') return true;
@@ -19,6 +22,10 @@ export const FormatPage = () => {
 
   return (
     <>
+      <WarningPopup
+        message="We don't support the landscape view."
+        isOpen={isOrientation}
+      />
       <Prompt message={confirmationHandler} />
       {isMobile ? <FormatPageMobile /> : <FormatPageDesktop />}
     </>
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useMobileFullScreen';
+export * from './useLandscape';
